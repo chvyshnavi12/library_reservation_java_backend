18917ec0b8609b696d0f04a43adf4b9eb31ce8b4
chvyshanvi <n210759@rguktn.ac.in>
2025-08-30 21:04:19 +0530

    Add files via upload
    files uploaded

---
+++ loginpage/src/main/java/com/login/loginpage/LoginpageApplication.java
@@ -0,0 +1,17 @@
+package com.login.loginpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginpageApplication.class, args);
+
+
+
+	}
+
+
+}
---
+++ loginpage/src/main/java/com/login/loginpage/vo/LoginRequest.java
@@ -0,0 +1,9 @@
+package com.login.loginpage.vo;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+     private String username;
+     private String password;
+
+}
